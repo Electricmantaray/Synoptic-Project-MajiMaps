8e09dc94f369061823b6c72c1b689a420574df7d
Electricmantaray <hlsj1066@gmail.com>
2025-05-30 12:38:41 +0100

    Finished initialising the home page and incorporating tailwing into our repository

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,5 +2,5 @@ import { getIndexData } from "../services/services.js";
 
 export const getIndex = async (req, res) => {
     const data = await getIndexData();
-  res.render("index", {indexData: data});
+  res.render("pages/index", {indexData: data});
 };
