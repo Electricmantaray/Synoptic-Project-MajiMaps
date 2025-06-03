ccb7e1cfd8ab9628822f4b6da30b35a18b84ae12
Electricmantaray <hlsj1066@gmail.com>
2025-06-03 03:28:28 +0100

    Update: header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,22 @@ module.exports = {
 
     theme: {
         extend: {
+            fontFamily: {},
+            fontSize: {},
+            fontWeight: {},
+            lineHeight: {},
+            letterSpacing: {},
+            borderRadius: {},
             colors: {
                 "text-colour": "#040316",
-      },
+            },
+            spacing: {},
+            width: {},
+            minWidth: {},
+            maxWidth: {},
+            height: {},
+            minHeight: {},
+            maxHeight: {},
         },
     },
 
