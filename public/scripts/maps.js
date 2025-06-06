44a3bd57670cdaed8bd3c8f72fabc434c8d143a8
Electricmantaray <hlsj1066@gmail.com>
2025-06-06 09:40:29 +0100

    Implementation: Seperated Hardcoded EJS into EJS and JSON

---
+++ public/scripts/maps.js
@@ -0,0 +1,49 @@
+
+// Map initialisation
+document.addEventListener('DOMContentLoaded', () => {
+    // Initialize the map centered roughly around Johannesburg
+    var map = L.map('map', {
+        scrollWheelZoom: false
+    }).setView([-26.2041028, 28.0473051], 15);
+
+    // Add OpenStreetMap tile layer with attribution
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    // TODO REFACTOR THIS
+    const coordsDisplay = document.getElementById('coordsPlaceholder');
+    const coordsEntry = document.getElementById('location');
+
+    let currentMarker = null;
+
+    map.on('click', function(e) {
+        const {lat, lng} = e.latlng;
+        if (currentMarker) {
+            map.removeLayer(currentMarker);
+        }
+        currentMarker = L.marker([lat, lng]).addTo(map);
+
+
+        coordsDisplay.textContent = `Latitude: ${lat.toFixed(6)}, Longitude: ${lng.toFixed(6)}`;
+        if (coordsEntry) coordsEntry.value = `${lat.toFixed(6)}, ${lng.toFixed(6)}`;    
+       
+    });
+
+    // Map Functionality
+    // TODO REFACTOR
+    const mapContainer = map.getContainer();
+
+
+    mapContainer.addEventListener('focus', () => {
+        map.scrollWheelZoom.enable();
+    });
+
+    mapContainer.addEventListener('blur', () => {
+        map.scrollWheelZoom.disable();
+    });
+
+});
+
+
