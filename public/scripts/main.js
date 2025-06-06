176ee61db0cda7b523c7077c9f06c6e72d2bacd3
Electricmantaray <hlsj1066@gmail.com>
2025-06-06 23:36:29 +0100

    Fix: added id to all forms

---
+++ public/scripts/main.js
@@ -0,0 +1,34 @@
+// Initialise main JS and form submission
+
+// Form Validation
+document.addEventListener("DOMContentLoaded", () => {
+    // === Report ===
+
+
+
+
+
+
+    
+    // === Contacts ===
+
+
+    // == Contact Form == 
+
+
+    // == Email Service Form == 
+    const emailServiceForm = document.querySelector('#emailService-form')
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
